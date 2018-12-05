7f5e9d16db07e2bdb803b63a7efc723de95c518b
chernand1 <42915407+chernand1@users.noreply.github.com>
2018-12-04 23:31:20 -0500

    Add files via upload
    Arduino sensor bridge I2C slave that connects to multiple analog and digital sensors. Used with raspberry pi

---
+++ Sensor.h
@@ -0,0 +1,166 @@
+#ifndef Sensor_h
+#define Sensor_h
+
+#include "Arduino.h"
+
+class Sensor
+{
+  public:
+    void sonarSetup(int trig, int echo);
+    float sonarMeasure();
+    float sonarDistance;
+    void tempHumidSetup(int dataHtPin);
+    float tempHumidData(int isTemp);
+    float returnHumid();
+    float returnTemp();
+    byte readHTByte();
+    float returnRegisterValue(int regAddress);
+    byte readI2CRegisters(int regAdress);
+    
+    int registerAddressRequest;
+    int byteCounter;
+    int numberOfLocationsToReturn;
+
+  private:
+  	int _trigPin;
+  	int _echoPin;
+    int _dataHtPin;
+
+    // Registers
+    // 
+    // 0 Temp + Temp decimal
+    // 1 Humid Data + Humid decimal
+    int _sonarOffset = 0;
+    int _tempOffset = 1;
+    float _sensorRegisters[10];
+    int _regCounter = 0;
+
+};
+
+void Sensor::sonarSetup(int trig, int echo)
+{
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+
+  _trigPin = trig;
+  _echoPin = echo;
+  
+  digitalWrite(_trigPin, LOW); 
+  sonarDistance = 0.0;
+
+  registerAddressRequest = 0;
+  numberOfLocationsToReturn = 1;
+}
+
+float Sensor::sonarMeasure()
+{
+  unsigned long startTimer = 0;
+  unsigned long endTimer = 0;
+  long duration = 0;
+  float distance_cm = 0.0;
+  
+  digitalWrite(_trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(_trigPin, LOW);
+  
+  duration = pulseIn(_echoPin, HIGH);
+  
+  sonarDistance = duration * (0.034/2);
+
+  _sensorRegisters[_sonarOffset] = sonarDistance;
+  
+  return sonarDistance;
+  
+}
+
+void Sensor::tempHumidSetup(int dataHtPin)
+{
+  pinMode(dataHtPin, OUTPUT);
+  _dataHtPin = dataHtPin;
+  digitalWrite(_dataHtPin, HIGH); 
+}
+
+float Sensor::tempHumidData(int isTemp)
+{
+long duration;
+
+  pinMode(_dataHtPin, OUTPUT);
+  digitalWrite(_dataHtPin, HIGH);
+  digitalWrite(_dataHtPin, LOW);
+  delay(30);
+  digitalWrite(_dataHtPin, HIGH);
+  delayMicroseconds(40);
+  pinMode(_dataHtPin, INPUT);
+
+  //Wait for device acknowledge
+  pulseIn(_dataHtPin, HIGH);
+  
+  int arrayByte[40];
+  int i;
+  int byteNo;
+  int Temperature[5];
+
+  for (byteNo=0; byteNo<5; byteNo++)
+  {
+    Temperature[byteNo] = readHTByte();
+  }
+
+  _sensorRegisters[_tempOffset] = Temperature[2]*1.0 + Temperature[3]*1.0/100;
+  _sensorRegisters[_tempOffset+1] = Temperature[0]*1.0 + Temperature[1]*1.0/100;
+
+  digitalWrite(_dataHtPin, HIGH);
+  pinMode(_dataHtPin, OUTPUT);
+  //digitalWrite(_dataHtPin, LOW);
+  digitalWrite(_dataHtPin, HIGH);
+  
+  if (isTemp != 0)
+    return _sensorRegisters[_tempOffset];
+  else
+    return _sensorRegisters[_tempOffset+1];
+}
+
+byte Sensor::readHTByte()
+{
+byte bytePad;
+
+bytePad = 0;
+
+  for (int i=0;i<8;i++)
+  {
+   if (pulseIn(_dataHtPin, HIGH) > 30)
+    bytePad = bytePad + (1 << (7-i));
+  }
+ 
+ return bytePad;
+}
+
+float Sensor::returnHumid()
+{
+  return _sensorRegisters[_tempOffset+1];
+}
+
+float Sensor::returnTemp()
+{
+  return _sensorRegisters[_tempOffset];
+}
+
+float Sensor::returnRegisterValue(int regAddress)
+{
+  return _sensorRegisters[regAddress];
+}
+
+byte Sensor::readI2CRegisters(int regAdress)
+{
+  float regFloatVal = returnRegisterValue(regAdress);
+  byte *regFloatByte = (byte *)&regFloatVal;
+  byte byteToReturn = regFloatByte[_regCounter];
+  
+  _regCounter++;
+
+  if (_regCounter == 3)
+    _regCounter = 0;
+  
+  return byteToReturn;
+  
+}
+#endif
---
+++ sensorBridge.ino
@@ -0,0 +1,51 @@
+#include "Sensor.h"
+#include "Wire.h"
+
+Sensor mySensor;
+
+void setup() {
+ 
+  Serial.begin(9600);
+  Serial.print("Hello world");
+  Wire.begin(12);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(sendEvent);
+  //mySensor.sonarSetup(2, 3);
+  mySensor.tempHumidSetup(4);
+  
+}
+
+// I2C SDA = A4, SCL = A5
+void receiveEvent(int howMany) {
+  
+  while (1 < Wire.available()) { // loop through all but the last
+    mySensor.registerAddressRequest = Wire.read(); // receive byte as a character
+    Serial.println(mySensor.registerAddressRequest);         // print the character
+  }
+  int x = Wire.read();    // receive byte as an integer
+  
+  Serial.println(x);         // print the integer
+  Serial.print("How many = ");
+  Serial.println(howMany);
+  
+}
+
+void sendEvent() {
+  
+  Wire.write(mySensor.readI2CRegisters(mySensor.registerAddressRequest));
+  
+}
+
+void loop() {
+  
+  Serial.print("Sensor Temp is =  ");
+  Serial.println(mySensor.tempHumidData(1));
+  Serial.println(mySensor.returnRegisterValue(1));
+  Serial.print("Sensor humid is =  ");
+  Serial.println(mySensor.returnHumid());
+  Serial.println(mySensor.returnRegisterValue(2));
+  delay(5000);
+ 
+}
+
+
